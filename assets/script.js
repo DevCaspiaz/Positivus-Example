b4c9254049bc53773561d5277bf7a519c0d9aebf
Eyvaz KARIMLI <devcaspiaz@gmail.com>
2025-02-06 07:24:44 +0300

    Section some components done

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const checkbox = document.getElementById("burger__menu");
+const body = document.body;
+const navMenu = document.querySelector(".nav__menu");
+
+checkbox.addEventListener("change", function () {
+    if (this.checked) {
+        body.style.overflow = "hidden";
+    } else {
+        body.style.overflow = "";
+    }
+});
+
+
+document.addEventListener("click", function (event) {
+    if (!navMenu.contains(event.target) && !checkbox.contains(event.target)) {
+        checkbox.checked = false;
+        body.style.overflow = "";
+    }
+});
